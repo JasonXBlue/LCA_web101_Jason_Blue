5d0305323366f806fbcdcf219c655de36782f5ca
JasonXBlue <JasonX.blue1@gmail.com>
2019-10-01 23:46:13 -0500

    10/1/19 11:46pm

--- myPortfolio/scripts/Todo.js
+++ myPortfolio/scripts/Todo.js
@@ -14,3 +14,23 @@ $(function() {
     $this.remove();
   });
 });
+
+/* Geolocation functions */
+
+var loc = document.getElementById("myLoc");
+
+function myLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    loc.innerHTML = "Not available.";
+  }
+}
+function showPosition(position) {
+  loc.innerHTML =
+    "Longitude: " +
+    position.coords.longitude +
+    "<br> Latitude: " +
+    position.coords.latitude +
+    "<br>";
+}
